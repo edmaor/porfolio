9d7c96da5c0b88a87131b0431d850d732a658e9d
Eduardo Martorell <edu@Javis-MacBook-Pro.local>
2023-06-10 13:17:07 +0200

    projects first version and deployment

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,11 +1,16 @@
 <template>
   <h1>My projects</h1>
-  div
-  <div id="myprojects" v-for="project in projectList" :key="project.id">
-    <ProjectCard :project="project"/>
+  <div id="myprojects">
+    <div v-for="project in projectList" :key="project.id">
+      <ProjectCard :project="project"/>
+    </div>
   </div>
   <hr>
   <h1>Other Programs</h1>
+  <div id="myprojects">
+    <div v-for="program in programs" :key="program.id">
+    </div>
+  </div>
 </template>
 
 <script>
@@ -15,7 +20,8 @@ export default {
   components: { ProjectCard },
     data() {
         return {
-            projectList: projects
+            projectList: projects,
+            programs: []
         }
     }
 }
