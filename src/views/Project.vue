4882011befd28b1238b108a5892c22daf5fbf29b
Eduardo Martorell <edu@Javis-MacBook-Pro.local>
2023-06-11 03:23:59 +0200

    pdf documentation on project details included

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,8 +1,13 @@
 <template>
+    <div class="app-header">
+        <template v-if="isLoading"> Loading... </template>
+    </div>
     <div v-if="project">
         <h1>{{ project.id }}</h1>
         <h3>{{ project.description }}</h3>
-        <vue-pdf-embed :source="project.documentation" width="80%"/>
+        <div id="p-documentation">
+            <vue-pdf-embed :source="documentationPath" @rendered="handleDocumentRender" :width="720"/>
+        </div>
     </div>
 </template>
 <script>
@@ -11,7 +16,9 @@ import projects from '../../resources/data/projects.json'
 export default {
     data() {
         return {
-            project: null
+            project: null,
+            isLoading: true,
+            documentationPath: ''
         }
     },
     components: {
@@ -20,11 +27,30 @@ export default {
     mounted() {
         const pid = this.$route.params.pid.toLowerCase();
         this.project = projects.find((proj) => proj.id.toLowerCase() === pid);
+        
+        this.documentationPath = 'https://raw.githubusercontent.com/edmaor/porfolio/d681b7701c48d0a96aabadfb6c2a5e61aa21c96c/resources/data/documentation/' + this.project.documentation
     },
+    methods: {
+    handleDocumentRender(args) {
+      console.log(args)
+      this.isLoading = false
+    },
+    handlePasswordRequest(callback, retry) {
+      callback(prompt(retry ? 'Enter password again' : 'Enter password'))
+    }
+  }
     
 }
 </script>
 
 <style>
-
+#p-documentation {
+    max-height: 1000px;
+    background: #2c3e50;
+    display: inline-block;
+    overflow: scroll;
+}
+#p-documentation > * {
+    margin: 32px 48px;
+}
 </style>
