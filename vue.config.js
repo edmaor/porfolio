7bb72230eacb81e5e0c4b6e2d95dba1f1f693c34
Eduardo Martorell <edu@Javis-MacBook-Pro.local>
2023-06-22 11:25:19 +0200

    router not working

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/projects/' : '/',
   transpileDependencies: true,
   outputDir: 'docs',
   chainWebpack: (config) => {
