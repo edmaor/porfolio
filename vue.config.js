4f3fb66db6e21e04e97ba5a75202d6461a1464c3
Eduardo Martorell <edu@Javis-MacBook-Pro.local>
2023-06-22 11:12:01 +0200

    Vue router configured to accept other routes when loading

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/projects/' : '/',
   transpileDependencies: true,
   outputDir: 'docs',
   chainWebpack: (config) => {
